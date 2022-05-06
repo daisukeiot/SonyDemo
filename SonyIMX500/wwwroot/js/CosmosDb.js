14d20aca0f29f05da266a0ae97a0c4d026f973b3
Daisuke Nakahara <daisuken@msn.com>
2022-05-05 18:39:11 -0700

    Clickable Cosmos DB Table

--- SonyIMX500/wwwroot/js/CosmosDb.js
+++ SonyIMX500/wwwroot/js/CosmosDb.js
@@ -40,9 +40,87 @@
         { name: "Device_ID", type: "text", align: "left" },
         { name: "Model_ID", type: "text", align: "left"},
         { name: "Image", type: "checkbox", align: "left", width: 50},
-        { name: "T", type: "text", align: "left"},
+        {
+            name: "T",
+            //itemTemplate: function (val, item) {
+            //    return $("<td>").text(val).on("click", function (evt) {
+            //        viewPhotoWithCosmosDbTable(item);
+            //    });
+            //},
+            type: "text",
+            align: "left"
+        },
         { name: "C", type: "text", align: "left", width: 50},
         { name: "P", type: "text", align: "left", width: 50 },
         { name: "Coord", type: "text", title: "(X,Y)-(x,y)", align: "left" }
-    ]
+    ],
+
+    rowClick: function (args) {
+        var $row = this.rowByItem(args.item);
+        viewPhotoWithCosmosDbTable(args.item);
+        $row.toggleClass("highlight");
+    },
 });
+
+function viewPhotoWithCosmosDbTable(item) {
+
+    var funcName = arguments.callee.name + "()";
+
+    if (item.Image == false) {
+        return;
+    }
+    var deviceId = item.Device_ID;
+
+    console.log(funcName);
+
+    try {
+        toggleLoader(false);
+
+        GetImage(deviceId, item.T)
+            .then((result) => {
+
+                if (result) {
+                    var json = JSON.parse(result);
+                    var canvas = document.getElementById("photoCanvas");
+                    var ctx = canvas.getContext('2d');
+                    var img = new Image();
+                    img.src = json.uri;
+                    img.onload = function () {
+                        ctx.drawImage(img, 0, 0)
+                        ctx.lineWidth = 3
+                        ctx.strokeStyle = "rgb(255, 255, 0)"
+                        ctx.font = '15px serif';
+                        ctx.fillStyle = "rgb(255, 255, 0)"
+                        ctx.textBaseline = "top";
+
+                        var coord_text = item.Coord;
+
+                        var coord_text_split = coord_text.split('-');
+
+                        var coord_text_slice = coord_text_split[0].replace(/\s+/g, '').slice(1, -1).split(",");
+
+                        var X = parseInt(coord_text_slice[0]);
+                        var Y = parseInt(coord_text_slice[1]);
+
+                        var coord_text_slice = coord_text_split[1].replace(/\s+/g, '').slice(1, -1).split(",");
+                        var x = parseInt(coord_text_slice[0]);
+                        var y = parseInt(coord_text_slice[1]);
+
+
+                        ctx.strokeRect(X, Y, x - X, y - Y);
+                        ctx.fillText(item.P, X + 5, Y + 5);
+                    }
+                }
+            })
+            .finally(() => {
+                toggleLoader(false);
+                var modal = document.getElementById("modalPhoto");
+                modal.style.display = "block";
+            });
+
+    } catch (err) {
+        msg = processError(funcName, err, true);
+        ret = false;
+        toggleLoader(true);
+    }
+}
